6576e3875576f112dafe1cc4d2e493651b319d40
SecLICE <SecLICE@seclice.com>
2026-05-09 20:11:58 +0100

    update

--- loader/src/main.c
+++ loader/src/main.c
@@ -151,7 +151,7 @@ int main(int argc, char **args)
 
     addrs_len = 1;
     addrs = calloc(addrs_len, sizeof(ipv4_t));
-    addrs[0] = inet_addr("192.168.1.57");
+    addrs[0] = inet_addr("172.31.26.48");
 
     if (argc == 2)
         id_tag = args[1];
@@ -163,7 +163,7 @@ int main(int argc, char **args)
     }
 
     if ((srv = server_create(sysconf(_SC_NPROCESSORS_ONLN), addrs_len, addrs, 
-                              1024 * 64, "192.168.1.57", 80, "192.168.1.57")) == NULL)
+                              1024 * 64, "172.31.26.48", 80, "172.31.26.48")) == NULL)
     {
         printf("Failed to initialize server. Aborting\n");
         return 1;
